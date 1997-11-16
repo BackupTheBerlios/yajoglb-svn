df838e882808f0ad7da6a820df05951c41f744a2
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
1997-11-16 02:53:15 +0000

    Added more functions.

--- trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/JNIInterface.c
+++ trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/JNIInterface.c
@@ -1,12 +1,15 @@
 /*
  * JNIInterface.c
  *
+ * $Id: JNIInterface.c,v 1.2 1997/11/16 02:53:15 razeh Exp $
+ *
  * This module handles convience routines for dealing with the Java
  * Native Interface.
  *
  * Copyright 1997
  * Robert Allan Zeh (razeh@balr.com)
  */
+
 
 #include "cygnusFixes.h"
 #include "ErrorHandling.h"
@@ -21,18 +24,46 @@ int lookupStaticIntField(JNIEnv *env, jclass class,
 			 const char *fieldName, int *fieldValue)
 
 {
-  jfieldID field; int returnCode = 0;
+  jfieldID field;
+  int returnCode = 0;
 
   field = (*env)->GetStaticFieldID(env, class, fieldName, "I");
   if (field == 0) {
     handleError(env, "OpenGL/OpenGLNativeException",
+		"Unable to get static field ID");
+    returnCode = 1;
+  } else {
+    *fieldValue =
+      (*env)->GetStaticIntField(env, class, field);
+  }
+
+  return returnCode;
+}
+
+
+
+/* Lookup a integer field in object and store the return value in
+   fieldValue.  If there is a problem getting the value fieldValue is
+   not changed and 1 is returned. Otherwise 0 is returned, and the
+   fieldValue is stored in *fieldValue. */
+int lookupIntField(JNIEnv *env, jobject object,
+		   const char *fieldName, int *fieldValue)
+{
+  jfieldID field;
+  jclass   objectClass;
+  int      returnCode = 0;
+
+  objectClass = (*env)->GetObjectClass(env, object);
+
+  field = (*env)->GetFieldID(env, objectClass, fieldName, "I");
+  if (field == 0) {
+    handleError(env, "OpenGL/OpenGLNativeException",
 		"Unable to get field ID");
     returnCode = 1;
+  } else {
+    *fieldValue = (*env)->GetIntField(env, objectClass, field);
   }
-  
-  *fieldValue =
-    (*env)->GetStaticIntField(env, class, field);
-  
+
   return returnCode;
 }
 
@@ -44,10 +75,9 @@ int lookupStaticIntField(JNIEnv *env, jclass class,
 int staticIntField(JNIEnv *env, jclass class,
 		   const char *fieldName)
 {
-  int fieldValue;
+  int fieldValue = -1;
 
-  if (lookupStaticIntField(env, class, fieldName, &fieldValue) != 0)
-    abort();
+  lookupStaticIntField(env, class, fieldName, &fieldValue);
 
   return fieldValue;
 }
@@ -74,8 +104,8 @@ getFullyQualifiedClass(JNIEnv *env,
 
 /* This returns the class name handed to it. */
 jclass getClass(JNIEnv *env,
-			const char *className,     /* The class to lookup. */
-			const char *errorString)   /* The error to print. */
+		const char *className,     /* The class to lookup. */
+		const char *errorString)   /* The error to print. */
 {
   return getFullyQualifiedClass(env, className, "OpenGL/OpenGLNativeException",
 				errorString);
@@ -83,3 +113,21 @@ jclass getClass(JNIEnv *env,
 
 
 
+/* This returns a methodID for the given class, method name and method
+   signature.  It will return NULL after calling handleError() if
+   something went wrong, and the methodID if things work out properly.  */
+jmethodID getMethodID(JNIEnv *env, jclass class,
+		      const char *methodName,
+		      const char *methodSignature,
+		      const char *errorMessage)
+{
+  jmethodID methodID;
+
+  methodID = (*env)->GetMethodID(env, class, 
+				 methodName, methodSignature);
+  if (methodID == 0) {
+    handleError(env, "OpenGL/OpenGLNativeException", errorMessage);
+    methodID = NULL;
+  }
+  return methodID;
+}
--- trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/JNIInterface.h
+++ trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/JNIInterface.h
@@ -1,8 +1,28 @@
+/*
+ * JNIInterface.h 
+ *
+ * $Id: JNIInterface.h,v 1.2 1997/11/16 02:53:15 razeh Exp $
+ *
+ * Copyright 1997
+ * Robert Allan Zeh (razeh@balr.com)
+ */
+
+
 #include <jni.h>
 
-int lookupStaticIntField(JNIEnv *env, jclass class,
+int lookupStaticIntField(JNIEnv *env, jclass class, 
 			 const char *fieldName, int *fieldValue);
+
+int lookupIntField(JNIEnv *env,  jobject object,
+		   const char *fieldName, int *fieldValue);
+
 int staticIntField(JNIEnv *env, jclass class, const char *fieldName);
+
 jclass getClass(JNIEnv *env,
 		const char *className,      /* The class to lookup. */
 		const char *errorString);   /* The error to print. */
+
+jmethodID getMethodID(JNIEnv *env, jclass class,
+		      const char *methodName,
+		      const char *methodSignature,
+		      const char *errorMessage);
