2d7c7a16f95c7ab3c9496eef21f077205d6e45b1
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
1998-09-10 01:00:27 +0000

    Added the versions comment.

--- trunk/YAJOGLB/DIR/OpenGL/CanvasSetupFailedException.java
+++ trunk/YAJOGLB/DIR/OpenGL/CanvasSetupFailedException.java
@@ -1,7 +1,7 @@
 /*
  * OpenGLCanvasSetupFailedException class
  *
- * $Id: CanvasSetupFailedException.java,v 1.1 1998/03/16 00:00:59 razeh Exp $
+ * $Id: CanvasSetupFailedException.java,v 1.2 1998/09/10 01:00:27 razeh Exp $
  *
  * Copyright 1998
  *
@@ -11,8 +11,10 @@
 package OpenGL;
 
 /** This exception is thrown when we have trouble setting up an OpenGLCanvas.
- 
-    @author Robert Allan Zeh (razeh@balr.com)
+ *
+ * @author Robert Allan Zeh (razeh@balr.com)
+ *
+ * @version 0.1
 */
 
 public class OpenGLCanvasSetupFailedException extends OpenGLNativeException {
