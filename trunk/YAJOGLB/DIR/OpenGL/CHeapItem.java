ef5b5e337fa3b0a753be7297996700e8206634a5
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
1998-09-10 00:55:37 +0000

    Added more comments.

--- trunk/YAJOGLB/DIR/OpenGL/CHeapItem.java
+++ trunk/YAJOGLB/DIR/OpenGL/CHeapItem.java
@@ -2,27 +2,28 @@
  *
  * CHeapItem
  *
+ * $Id: CHeapItem.java,v 1.4 1998/09/10 00:55:37 razeh Exp $
+ *
  * Copyright 1997
  * Robert Allan Zeh (razeh@balr.com)
  */
 
 package OpenGL;
 
-/**
- * CHeapItem (pun intended) <P>
- *
+import java.util.Hashtable;
+
+/** 
  * This abstract class maintains a reference to a pointer in the C
  * memory heap.  We keep track of the pointer with an integer, and
- * expect to have a method that allocates the item (<a
- * href="#obtainCHeapItem">obtainCHeapItem</a>) and another method
- * that frees the item (<a href="#freeCHeapItem">freeCHeapItem</a>).
- * We call the allocation method in our base construcor and call the
- * free method in our finalizer.
+ * expect to have a method that allocates the item (
+ * obtainCHeapItem) and another method that frees the item
+ * (freeCHeapItem).  We call the allocation method in our base
+ * constructor and call the free method in our finalizer.
  *
  * @author Robert Allan Zeh (razeh@balr.com)
  *
  * @version 0.1
- *
+ * 
  */
 
 public class CHeapItem {
@@ -30,10 +31,10 @@ public class CHeapItem {
   private int cHeapPointer;
 
   /** This returns the integer representation of our pointer.  
-    
-    @exception IllegalAccessException If our pointer has not
+    @return the integer representation of our pointer
+    @exception IllegalStateException If our pointer has not
     been set yet*/
-  public int heapPointer() throws IllegalAccessException {
+  public int heapPointer() throws IllegalStateException {
     if (cHeapPointer == 0) {
       /* Uh-oh.  Our heap pointer was never set, which is bad. */
       throw new IllegalStateException("Asking for an unset heap pointer.");
@@ -41,39 +42,37 @@ public class CHeapItem {
     return cHeapPointer;
   }
 
-  /** Class constructor.  The C heap item is allocated immediately. */
+  /** Class constructor. */
   public CHeapItem() {
-    setCHeapItem();
+    setCHeapItem(null);
   }
 
-  /** Class constructor with the option of delaying the C heap allocation.  
-   *
-   * @param avoidSetCheapItem if true, delay the C heap item's allocation until setCHeapItem is invoked
-   */
-
-  public CHeapItem(boolean avoidSetCHeapItem) {
-    if (avoidSetCHeapItem == false)
-      setCHeapItem();
+  /** Class constructor, with optional arguments (in a Hashtable) that
+      are passed to setCHeapItem. */
+  public CHeapItem(Hashtable optionalArguments) {
+    setCHeapItem(optionalArguments);
   }
+  
 
   /** This is used to set our C heap item based on our call to
-   obtainCHeapItem.  If called more than once it will throw an
-   IllegalStateException. */
-  private void setCHeapItem() {
+      obtainCHeapItem.
+   @exception IllegalStateException thrown if called more than once. */
+  protected void setCHeapItem(Hashtable optionalArguments) {
     if (cHeapPointer != 0) {
       throw new IllegalStateException("Unable to call setCHeapItem twice.");
     }
-    cHeapPointer = obtainCHeapItem();
+    cHeapPointer = obtainCHeapItem(optionalArguments);
     if (0 == cHeapPointer) {
       throw new OutOfMemoryError();
     }
   }
   
-  /** This is the method subclasses should use to implement obtaining
+  /** The method subclasses should use to implement obtaining
       an item from the C heap.  They should return 0 if they are
-      unable to obtain the item. */
-  private int obtainCHeapItem() {
-    return 0;
+      unable to obtain the item. 
+      @return the integer representation of our C heap pointer. */
+  protected int obtainCHeapItem(Hashtable optionalArguments) {
+    throw new RuntimeException("The root obtainCHeapItem method was called, when it should have been overriden by a subclass");
   }
 
   /** This is the method subclasses should use to implement freeing
@@ -83,7 +82,7 @@ public class CHeapItem {
       @param heapItem the C heap item to free.
 
       */
-  private void freeCHeapItem(int heapItem) {
+  protected void freeCHeapItem(int heapItem) {
     ;
   }
 
