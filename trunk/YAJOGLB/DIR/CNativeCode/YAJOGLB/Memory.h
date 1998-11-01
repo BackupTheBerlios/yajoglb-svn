c321e7da40f27a47576d9d26a14a899ff28b7cc2
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
1998-11-01 21:44:18 +0000

    memory handling.

--- trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/Memory.h
+++ trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/Memory.h
@@ -1,3 +1,12 @@
+/*
+ * memory.h
+ *
+ * $Id: Memory.h,v 1.2 1998/11/01 21:44:18 razeh Exp $
+ *
+ * Copyright 1998
+ * Robert Allan Zeh (razeh@balr.com)
+ */
+
 #include <stdlib.h>
 
 extern void *privateMalloc(size_t size);
