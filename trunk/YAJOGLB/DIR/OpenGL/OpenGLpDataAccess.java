dbaf97ff9a04455212350fd64a365eb0b1fbd39e
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
1998-11-09 01:07:51 +0000

    Fixed the comment.

--- trunk/YAJOGLB/DIR/OpenGL/OpenGLpDataAccess.java
+++ trunk/YAJOGLB/DIR/OpenGL/OpenGLpDataAccess.java
@@ -1,14 +1,14 @@
 /*
  * OpenGLpDataAccess
  * 
- * $Id: OpenGLpDataAccess.java,v 1.1 1998/09/10 01:06:04 razeh Exp $
+ * $Id: OpenGLpDataAccess.java,v 1.2 1998/11/09 01:07:51 razeh Exp $
  * 
  */
 
 package OpenGL;
 
 public interface OpenGLpDataAccess {
-  /* Gets the secret window information! */
+  /** Gets the WIN32 specific window information. */
   public int getInfo(java.awt.Graphics g);
   public int getHWnd(java.awt.Canvas canvas);
   public int getHDC(java.awt.Canvas canvas);
