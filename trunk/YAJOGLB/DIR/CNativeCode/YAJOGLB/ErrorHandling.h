bcaac8d1a300200765ceaef749fc0137e75db5cb
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
1998-12-23 00:43:37 +0000

    Added a function to handle errors that can not be recovered from.

--- trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/ErrorHandling.h
+++ trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/ErrorHandling.h
@@ -10,6 +10,8 @@
 void handleError(JNIEnv *env, 
 		 const char *exceptionClass, const char *errorMessage);
 void log(JNIEnv *env, const char *string);
+void fatalUnreportableError(const char *errorString);
 
 #define NATIVE_PROBLEM -1
-
+#define OPENGL_NATIVE_EXCEPTION "OpenGL/OPENGL_NATIVE_EXCEPTION"
+#define OPENGL_NATIVE_MEMORY_EXHAUSTED_EXCEPTION "OpenGL/OpenGLNativeMemoryExhaustedException"
