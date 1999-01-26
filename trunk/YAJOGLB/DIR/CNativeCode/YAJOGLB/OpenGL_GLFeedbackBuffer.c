e207da16a38bbaf7436389c7cd9c2e5d2287ebd1
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
1999-01-26 23:55:44 +0000

    Moved some of the feedback functions into an abstract class.

--- trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/OpenGL_GLFeedbackBuffer.c
+++ trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/OpenGL_GLFeedbackBuffer.c
@@ -1,9 +1,9 @@
 /*
- * OpenGL_GL.c
+ * OpenGL_GLFeedbackBuffer.c
  *
- * $Id: OpenGL_GLFeedbackBuffer.c,v 1.1 1998/11/04 00:29:23 razeh Exp $
+ * $Id: OpenGL_GLFeedbackBuffer.c,v 1.2 1999/01/26 23:55:44 razeh Exp $
  *
- * This implements the generic GL methods.
+ * This implements the native methods for the feedback buffer.
  *
  * Copyright 1998
  * Robert Allan Zeh (razeh@balr.com)
@@ -15,77 +15,15 @@
 #include <GL/gl.h>
 
 #include "cygnusFixes.h"
-#include "ErrorHandling.h"
 #include "OpenGL_GLFeedbackBuffer.h"
-#include "memory.h"
-#include "JNIInterface.h"
 
 /* This file contains the C implementation for the feedback buffer.  The actual
 buffer needs to be mapped into a persistent array so that OpenGL can write into it is called.
+All of the persistence handling is taken care of by our Java superclass.
 */
 
-/*
- * Class:     OpenGL_GLFeedbackBuffer
- * Method:    feedbackBuffer
- * Signature: (III)V
- */
 JNIEXPORT void JNICALL Java_OpenGL_GLFeedbackBuffer_feedbackBuffer
   (JNIEnv *env, jobject feedbackBuffer, jint size, jint type, jint bufferPointer)
 {
 	glFeedbackBuffer(size, type, (GLfloat*) bufferPointer);
-}
-
-
-
-/*
- * Class:     OpenGL_GLFeedbackBuffer
- * Method:    mapBuffer
- * Signature: ([F)I
- */
-JNIEXPORT jint JNICALL Java_OpenGL_GLFeedbackBuffer_mapBuffer
-  (JNIEnv *env, jobject feedbackBuffer, jfloatArray jbuffer)
-{
-	GLfloat  *buffer;
-	jboolean  isCopy;
-	jmethodID setNativeArrayIsCopyMethod;
-	jclass    feedbackClass;
-	
-	buffer = (*env)->GetFloatArrayElements(env, jbuffer, &isCopy);
- 
-	feedbackClass = (*env)->GetObjectClass(env, feedbackBuffer);
-
-	setNativeArrayIsCopyMethod = getMethodID(env, feedbackClass,
-	      "setNativeArrayIsCopy", "(Z)V", "Unable to find setNativeArrayIsCopy");
-	if (setNativeArrayIsCopyMethod) {
-			(*env)->CallVoidMethod(env, feedbackBuffer, 
-				                     setNativeArrayIsCopyMethod, isCopy);
-	}
-
-	return (int) buffer;
-}
-
-
-
-/*
- * Class:     OpenGL_GLFeedbackBuffer
- * Method:    unmapBuffer
- * Signature: ([FI)V
- */
-JNIEXPORT void JNICALL Java_OpenGL_GLFeedbackBuffer_unmapBuffer
-  (JNIEnv *env, jobject feedbackBuffer, jfloatArray jbuffer, jint buffer)
-{
-	(*env)->ReleaseFloatArrayElements(env, jbuffer, (float*)buffer, 0);
-}
-
-
-
-/*
- * Class:     OpenGL_OpenGLFeedbackBuffer
- * Method:    copyBuffer
- * Signature: ([FI)V
- */
-JNIEXPORT void JNICALL Java_OpenGL_GLFeedbackBuffer_copyBuffer
-  (JNIEnv *env, jobject feedbackBuffer, jfloatArray jbuffer, jint buffer)
-{
-	(*env)->ReleaseFloatArrayElements(env, jbuffer, (float*)buffer, JNI_COMMIT);
 }
