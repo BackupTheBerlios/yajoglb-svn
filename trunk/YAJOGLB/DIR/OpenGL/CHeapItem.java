0d153d0c680b2930506278670cd93cd3f1288df9
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
1999-01-27 00:00:11 +0000

    Made the class abstract.

--- trunk/YAJOGLB/DIR/OpenGL/CHeapItem.java
+++ trunk/YAJOGLB/DIR/OpenGL/CHeapItem.java
@@ -2,7 +2,7 @@
  *
  * CHeapItem
  *
- * $Id: CHeapItem.java,v 1.5 1998/11/01 02:16:07 razeh Exp $
+ * $Id: CHeapItem.java,v 1.6 1999/01/27 00:00:11 razeh Exp $
  *
  * Copyright 1997
  * Robert Allan Zeh (razeh@balr.com)
@@ -15,18 +15,18 @@ import java.util.Hashtable;
 /** 
  * This abstract class maintains a reference to a pointer in the C
  * memory heap.  We keep track of the pointer with an integer, and
- * expect to have a method that allocates the item (
- * obtainCHeapItem) and another method that frees the item
- * (freeCHeapItem).  We call the allocation method in our base
- * constructor and call the free method in our finalizer.
+ * expect to have a method that allocates the item
+ * ( <code>obtainCHeapItem</code>) and another method that frees the
+ * item (<code>freeCHeapItem</code>).  We call the allocation method
+ * in our base constructor and call the free method in our finalizer.
  *
  * @author Robert Allan Zeh (razeh@balr.com)
  *
  * @version 0.1
- * 
+ *
  */
 
-class CHeapItem {
+abstract class CHeapItem {
   /** This is the integer representation of our pointer. */
   private int cHeapPointer;
 
@@ -34,7 +34,7 @@ class CHeapItem {
     @return the integer representation of our pointer
     @exception IllegalStateException If our pointer has not
     been set yet*/
-  public int heapPointer() throws IllegalStateException {
+  int heapPointer() throws IllegalStateException {
     if (cHeapPointer == 0) {
       /* Uh-oh.  Our heap pointer was never set, which is bad. */
       throw new IllegalStateException("Asking for an unset heap pointer.");
@@ -47,17 +47,18 @@ class CHeapItem {
     setCHeapItem(null);
   }
 
-  /** Class constructor, with optional arguments (in a Hashtable) that
-      are passed to setCHeapItem. */
+  /** Class constructor, with optional arguments (in a
+      <code>Hashtable</code>) that are passed to
+      <code>setCHeapItem</code>. */
   public CHeapItem(Hashtable optionalArguments) {
     setCHeapItem(optionalArguments);
   }
   
 
   /** This is used to set our C heap item based on our call to
-      obtainCHeapItem.
+      <code>obtainCHeapItem</code>.
    @exception IllegalStateException thrown if called more than once. */
-  protected void setCHeapItem(Hashtable optionalArguments) {
+  void setCHeapItem(Hashtable optionalArguments) {
     if (cHeapPointer != 0) {
       throw new IllegalStateException("Unable to call setCHeapItem twice.");
     }
@@ -71,20 +72,14 @@ class CHeapItem {
       an item from the C heap.  They should return 0 if they are
       unable to obtain the item. 
       @return the integer representation of our C heap pointer. */
-  protected int obtainCHeapItem(Hashtable optionalArguments) {
-    throw new RuntimeException("The root obtainCHeapItem method was called, when it should have been overriden by a subclass.");
-  }
+  abstract int obtainCHeapItem(Hashtable optionalArguments);
 
   /** This is the method subclasses should use to implement freeing
       the item from the C heap.  It will be called from our finalize
       method. 
-      
       @param heapItem the C heap item to free.
-
-      */
-  protected void freeCHeapItem(int heapItem) {
-    ;
-  }
+  */
+  abstract void freeCHeapItem(int heapItem);
 
   /** Invokes freeCheapItem to release our underlying
     C heap item. <P>
