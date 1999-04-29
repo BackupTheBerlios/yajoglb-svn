57883616d7299ef649118ac2f715af9994c5a314
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
1999-04-29 01:25:34 +0000

    More linux changes.

--- trunk/YAJOGLB/DIR/sun/awt/motif/X11pDataAccess.java
+++ trunk/YAJOGLB/DIR/sun/awt/motif/X11pDataAccess.java
@@ -1,5 +1,5 @@
 /*
- * $Id: X11pDataAccess.java,v 1.1 1999/02/13 18:52:14 razeh Exp $
+ * $Id: X11pDataAccess.java,v 1.2 1999/04/29 01:25:34 razeh Exp $
  *
  * Taken (and then modified) from jogl-0.7
  *
@@ -12,15 +12,25 @@ package sun.awt.motif;
 
 import sun.awt.*;
 import OpenGL.OpenGLpDataAccess;
+import OpenGL.OpenGLCanvas;
 import java.awt.Canvas;
 import java.awt.Graphics;
 
 /**
  * This class is used to get to the raw X11 pointers used by Java for 
  * peer window support. 
+ *
+ * @author Robert Allan Zeh (razeh@balr.com)
+ *
+ * @version 0.3
  */
 public class X11pDataAccess implements OpenGLpDataAccess {
-  /** This returns the X11 drawing surface for a given graphics object. */
+  /** The Display that we setup for the OpenGLCanvas we belong to. 
+   @return the display for our OpenGLCanvas. */
+  private int privateDisplay;
+  
+  /** This returns the X11 drawing surface for a given graphics object. 
+   @return the X11 drawing surface for canvas. */
   private X11DrawingSurface getDrawingSurface (java.awt.Canvas canvas) {
       MCanvasPeer cp = (MCanvasPeer) canvas.getPeer();
       X11DrawingSurface xds = (X11DrawingSurface) cp.getDrawingSurfaceInfo();
@@ -28,24 +38,58 @@ public class X11pDataAccess implements OpenGLpDataAccess {
       return xds;
   }
 
-  public int getDisplay(Canvas canvas) {
-      return getDrawingSurface(canvas).getDisplay();
+  /** Returns the Display we setup for our canvas. 
+      @return the Display previously set for our canvas. */
+  public int getDisplay() {
+    return privateDisplay;
   }
 
+  /** Sets the Display we setup for our canvas. 
+      @param display our new display that will be returned by getDisplay. */
+  public void setDisplay(int display) {
+    privateDisplay = display;
+  }
+
+  /** Returns the drawable for our canvas.
+      @param canvas the canvas whose color map we want
+      @return the drawable for canvas. */
   public int getDrawable(Canvas canvas) {
       return getDrawingSurface(canvas).getDrawable();
   }
 
+  /** Returns the color map ID for canvas. 
+      @param canvas the canvas whose color map we want
+      @return the color map ID for canvas. */
   public int getColormapID(Canvas canvas) {
       return getDrawingSurface(canvas).getColormapID();
   }
   
-  /* This accessors are really for win32. */
+  /** This accessors is really for win32, and simply returns 0.
+     @param canvas ignored
+     @return always 0. */
   public int getHWnd(java.awt.Canvas canvas) {
     return 0;
   }
 
+  /* This accessor is really here for win32, and simply returns 0. 
+     @param canvas ignored
+     @return always 0. */
   public int getHDC(java.awt.Canvas canavs) {
     return 0;
   }
+
+
+
+  /** Invoked by our OpenGLCanvas when it attaches to us. */
+  public void attach(OpenGLCanvas canvas) {
+    
+  }
+
+
 }
+
+
+
+
+
+
