7454791e7505df0f3463bb737c02ce4993f834e3
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
2001-07-06 23:42:57 +0000

    Added the LGPL header and canvas locking.

--- trunk/YAJOGLB/DIR/OpenGL/Canvas.java
+++ trunk/YAJOGLB/DIR/OpenGL/Canvas.java
@@ -1,10 +1,32 @@
 /*
- * OpenGLCanvas class
+  Provides a canvas to render OpenGL into.
+ 
+  Copyright 2001, Robert Allan Zeh (razeh@yahoo.com)
+  7346 Lake Street #3W
+  River Forest, IL 60305
+ 
+  This library is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA
+
+*/
+
+/*
+ * Canvas class
  *
- * $Id: Canvas.java,v 1.6 1999/04/29 01:17:14 razeh Exp $
+ * $Id: Canvas.java,v 1.7 2001/07/06 23:42:57 razeh Exp $
  *
- * Copyright 1998
- * Robert Allan Zeh (razeh@balr.com)
  */
 
 package OpenGL;
@@ -14,17 +36,16 @@ import java.lang.Class;
 
 /** This provides an embedded object that OpenGL can render into.  It
  * is similar to a normal Canvas object, but it can be setup with
- * OpenGLCapabilities that describe how OpenGL should render into the
+ * Capabilities that describe how OpenGL should render into the
  * canvas.  For example, the precision of the depth buffer is
- * specified via the OpenGLCapabilities.  If no capabilities are
- * specified, a default capabilities object will be used.  
+ * specified via the Capabilities.  If no capabilities are specified,
+ * a default capabilities object will be used.
  *
- * @author Robert Allan Zeh (razeh@balr.com)
+ * @author Robert Allan Zeh (razeh@yahoo.com)
  *
- * @version 0.3
- */
+ * @version 0.4 */
 
-public class OpenGLCanvas extends Canvas {
+abstract public class Canvas extends java.awt.Canvas {
   
   /** Make sure that we load in our native library. */
   static {
@@ -32,57 +53,57 @@ public class OpenGLCanvas extends Canvas {
   }
 
   /** This holds the capabilities we will request of our display. */
-  private OpenGLCapabilities capabilities;
+  private Capabilities capabilities;
 
   /** True if we have called our native setup code. */
   private boolean canvasHasBeenSetup = false;
 
-  public OpenGLCanvas() {
+  public Canvas() {
     super();
 
+    /*
     try {
       Class dataAccessClass = Class.forName(dataAccessClass());
       dataAccess = (OpenGLpDataAccess)dataAccessClass.newInstance();
-      dataAccess.attach(this);
     } catch (java.lang.InstantiationException exception) {
-      throw new OpenGLNativeException("Unable to create a data access instance: " +
+      throw new NativeException("Unable to create a data access instance: " +
 				      exception);
     } catch (java.lang.ClassNotFoundException exception) {
-      throw new OpenGLNativeException("Unable to create a data access instance: " +
+      throw new NativeException("Unable to create a data access instance: " +
 				      exception);
     } catch (java.lang.IllegalAccessException exception) {
-      throw new OpenGLNativeException("Unable to create a data access instance: " +
+      throw new NativeException("Unable to create a data access instance: " +
 				      exception);
     }
-
+    */
     setCapabilities(defaultCapabilities());
   }
 
-  private native boolean setupOpenGLCanvas();
+  private native boolean setupCanvas();
 
-  /** The default OpenGLCapabilities object that we return at startup
+  /** The default Capabilities object that we return at startup
       for our display. */
-  private OpenGLCapabilities defaultCapabilities() {
-    return OpenGLCapabilities.defaultCapabilities();
+  private Capabilities defaultCapabilities() {
+    return Capabilities.defaultCapabilities();
   }
 
   /** Sets the capabilities that this widget will use when we are
      opened up.  This has no effect once the canvas has been displayed
      on screen. */
-  protected void setCapabilities(OpenGLCapabilities newCapabilities) {
+  protected void setCapabilities(Capabilities newCapabilities) {
     capabilities = newCapabilities;
   }
 
   /** Returns the capabilities object that was used when we started
       our display. */
-  protected OpenGLCapabilities capabilities() {
+  protected Capabilities capabilities() {
     return capabilities;
   }
 
   /** This handles the actual setup for our OpenGL canvas. */
   private void init() {
-    if (false == setupOpenGLCanvas()) {
-      throw new OpenGLCanvasSetupFailedException("Unable to setup the canvas.");
+    if (false == setupCanvas()) {
+      throw new CanvasSetupFailedException("Unable to setup the canvas.");
     }
   }
 
@@ -93,9 +114,7 @@ public class OpenGLCanvas extends Canvas {
 
   /** The method that subclasses should override for rendering.  It is 
    invoked by <code>paint(Graphics g)</code>. */
-  public void paint() {
-
-  }
+  abstract public void paint();
 
   /** This is overriden to handle our canvas setup, and calling
       glInit() and paint(). This is final to prevent any subclasses
@@ -103,17 +122,19 @@ public class OpenGLCanvas extends Canvas {
       because doing so leaves our display set to the foreground color
       after redisplays under X.  */
   final public void paint(Graphics g) {
-
-    // If we have not been setup, set ourself up.
-    if (false == canvasHasBeenSetup) {
-      init();
-      glInit();
-      // Make sure the setup only happens once.
-      canvasHasBeenSetup = true;
-    }
-    paint();
+      // If we have not been setup, set ourself up.
+      if (false == canvasHasBeenSetup) {
+	  init();
+	  glInit();
+	  // Make sure the setup only happens once.
+	  canvasHasBeenSetup = true;
+      }
+      paint();
   }
 
+  protected native void lockCanvas();
+  protected native void unlockCanvas();
+  
   /** This contains the native call needed to actually swap our display
       buffer. */
   private native void nativeSwapBuffers();
@@ -140,10 +161,4 @@ public class OpenGLCanvas extends Canvas {
   int getHDC() {
     return dataAccess.getHDC(this);
   }
-
 }
-
-
-
-
-
