8de9e3dc8899fd67347e6554367456be0e5a9de9
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
2001-07-06 23:41:30 +0000

    Added the JAWT.

--- trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/win32DCDictionary.h
+++ trunk/YAJOGLB/DIR/CNativeCode/YAJOGLB/win32DCDictionary.h
@@ -1,12 +1,27 @@
 /*
  * win32DCDictionary.h
  *
- * $Id: win32DCDictionary.h,v 1.1 1998/11/01 21:44:18 razeh Exp $
+ * $Id: win32DCDictionary.h,v 1.2 2001/07/06 23:41:30 razeh Exp $
  *
  * Copyright 1998
- * Robert Allan Zeh (razeh@balr.com)
+ * Robert Allan Zeh (razeh@yahoo.com)
  */
+
+
 
 #include <windows.h>
 
-HDC getDCForCanvas(JNIEnv *env, jobject canvas);
+#ifdef USE_JAWT
+#include "jawt_md.h"
+#endif
+
+typedef struct {
+	HDC hDC;
+#ifdef USE_JAWT
+	JAWT_DrawingSurface *ds;
+	JAWT_DrawingSurfaceInfo *dsi;
+#endif
+} CanvasInfo;
+
+CanvasInfo getCanvasInfo(JNIEnv *env, jobject canvas);
+void freeCanvasInfo(JNIEnv *env, CanvasInfo info);
