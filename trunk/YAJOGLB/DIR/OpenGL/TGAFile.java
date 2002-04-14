e8fd232cd221f9d6c7fa4107d88cc45913dc6243
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
2002-04-14 18:18:01 +0000

    Header file updates.

--- trunk/YAJOGLB/DIR/OpenGL/TGAFile.java
+++ trunk/YAJOGLB/DIR/OpenGL/TGAFile.java
@@ -1,3 +1,26 @@
+/*
+  An easy way to load TGA files.
+ 
+  Copyright 2001, Robert Allan Zeh (razeh@yahoo.com)
+  7346 Lake Street #3W
+  River Forest, IL 60305
+ 
+  This library is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA
+
+*/
 package OpenGL;
 
 import java.io.*;
@@ -5,9 +28,9 @@ import java.awt.Dimension;
 
 /** Supports the reading and writing of simple TGA files.
  *
- * @author Robert Allan Zeh (razeh@balr.com)
+ * @author Robert Allan Zeh (razeh@yahoo.com)
  *
- * @version 0.3
+ * @version 0.4
  */
 
 
@@ -16,7 +39,7 @@ public class TGAFile {
    * @param gl the OpenGL context to use for our readPixels call.
    * @param canvas the canvas we use to create our TGA object from.
    */
-  public TGAFile(GL gl, OpenGLCanvas canvas) {
+  public TGAFile(GL gl, Canvas canvas) {
     setSize(new Dimension(canvas.getSize().width, canvas.getSize().height));
     data = (byte[]) gl.readPixels(0, 0, getSize().width, getSize().height, 
 				  GLConstants.RGBA, GLConstants.GL_UNSIGNED_BYTE);
