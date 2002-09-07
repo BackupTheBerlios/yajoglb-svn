2f7cff30f0c1af1cad5b1152b3bce15352a1f74d
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
2002-09-07 17:22:06 +0000

    We now use a MatrixTransformer to offset the spheres.

--- trunk/YAJOGLB/DIR/tests/Concurrence.java
+++ trunk/YAJOGLB/DIR/tests/Concurrence.java
@@ -1,7 +1,7 @@
 /*
  * Concurrence
  *
- * $Id: Concurrence.java,v 1.5 2002/04/06 15:12:14 razeh Exp $
+ * $Id: Concurrence.java,v 1.6 2002/09/07 17:22:06 razeh Exp $
  *
  * Copyright 1998
  *
@@ -18,28 +18,36 @@ import OpenGL.*;
 public class Concurrence {
 
   public static void main(String args[]) {
+    Sphere         sphere1 = new Sphere();
+    Sphere         sphere2 = new Sphere();
     PlainCube      cube    = new PlainCube();
     GeometryViewer viewer1 = new GeometryViewer();
     GeometryViewer viewer2 = new GeometryViewer();
     ExitableFrame  frame1  = new ExitableFrame();
     Frame          frame2  = new Frame();
-    Sphere         sphere1 = new Sphere();
-    Sphere         sphere2 = new Sphere();
+
+    /* We use this transformation matrix to move the
+     * spheres off to the side. */
+    double translation[] = {  1.0, 0.0, 0.0, 0.0, 
+			      0.0, 1.0, 0.0, 0.0,
+			      0.0, 0.0, 1.0, 0.0,
+			      -2.0, 0.0, 0.0, 1.0 };
 
     viewer1.addElement(cube);
-    viewer1.addElement(sphere1);
     viewer1.addElement(new TurningAxis());
+    viewer1.addElement(new MatrixTransform(translation, sphere1));
     frame1.setLayout(new GridLayout(1,1));
     frame1.add(viewer1);
     frame1.setTitle("Thread #1");
     frame1.pack();
     frame1.setSize(new Dimension(400,400));
+
     frame1.setBackground(java.awt.Color.black);
     frame1.setVisible(true);
 
     viewer2.addElement(cube);
-    viewer2.addElement(sphere2);
     viewer2.addElement(new TurningAxis());
+    viewer2.addElement(new MatrixTransform(translation, sphere2));
     frame2.setLayout(new GridLayout(1,1));
     frame2.add(viewer2);
     frame2.setTitle("Thread #2");
