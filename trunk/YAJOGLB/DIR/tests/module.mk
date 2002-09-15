57ea2a63e905871e3c8f96433868aaffcc0e3788
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
2002-09-15 16:04:00 +0000

    Slight cleanup.

--- trunk/YAJOGLB/DIR/tests/module.mk
+++ trunk/YAJOGLB/DIR/tests/module.mk
@@ -1,23 +1,23 @@
 DISTRIBUTED_FILES += tests/ogl.tga tests/run.sh tests/Readme.html
 
-JAVA_SRC += tests/VertexPointer.java tests/Stretch.java					\
-	    tests/NurbsSurface.java							\
-            tests/GeometryViewer.java tests/GeometryObject.java				\
-	    tests/PlainCube.java							\
-	    tests/Vec.java tests/OpenGLCanvasTest.java tests/Concurrence.java		\
-	    tests/ExitableFrame.java tests/MatrixModifiers.java				\
-	    tests/Sphere.java								\
-	    tests/Feedback.java tests/Axis.java						\
-            tests/TexturedCube.java tests/Simplest.java					\
-	    tests/TessVertex.java tests/QuadricError.java				\
-	    tests/NurbsCallback.java tests/Select.java					\
-	    tests/TurningAxis.java tests/InfoPanel.java					\
-	    tests/StretchTexture.java tests/OBJ.java tests/ObjFace.java			\
-	    tests/ObjRender.java tests/ObjVertex.java tests/ObjReader.java		\
-	    tests/ObjMaterialReader.java tests/ObjMaterial.java				\
-	    tests/ObjNormal.java tests/MatrixTransform.java				\
-	    tests/ObjFormatException.java tests/ObjDrawable.java			\
-	    tests/FrameRatePanel.java							\
+JAVA_SRC += tests/VertexPointer.java tests/Stretch.java				\
+	    tests/NurbsSurface.java						\
+            tests/GeometryViewer.java tests/GeometryObject.java			\
+	    tests/PlainCube.java						\
+	    tests/Vec.java tests/OpenGLCanvasTest.java tests/Concurrence.java	\
+	    tests/ExitableFrame.java tests/MatrixModifiers.java			\
+	    tests/Sphere.java							\
+	    tests/Feedback.java tests/Axis.java					\
+            tests/TexturedCube.java tests/Simplest.java				\
+	    tests/TessVertex.java tests/QuadricError.java			\
+	    tests/NurbsCallback.java tests/Select.java				\
+	    tests/TurningAxis.java tests/InfoPanel.java				\
+	    tests/StretchTexture.java tests/OBJ.java tests/ObjFace.java		\
+	    tests/ObjRender.java tests/ObjVertex.java tests/ObjReader.java	\
+	    tests/ObjMaterialReader.java tests/ObjMaterial.java			\
+	    tests/ObjNormal.java tests/MatrixTransform.java			\
+	    tests/ObjFormatException.java tests/ObjDrawable.java		\
+	    tests/FrameRatePanel.java						\
 	    tests/GLUTShapes.java tests/TexturedSphere.java
 
 
