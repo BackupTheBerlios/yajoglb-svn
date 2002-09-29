7e094cfca362705222baacdc9806d412031175d0
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
2002-09-29 14:43:18 +0000

    Added the directory seperator.

--- trunk/YAJOGLB/DIR/native.mk
+++ trunk/YAJOGLB/DIR/native.mk
@@ -15,5 +15,5 @@ $(NATIVECODEDIR)/OpenGL_%.h: $(PACKAGEDIR)/%.class
 OBJS := $(CSRCS:.c=.o)
 
 %.d: %.c
-	bin$(DIR_SEPARATOR)/depend.py $(CFLAGS) $< > $@
+	bin$(DIR_SEPARATOR)depend.py $(CFLAGS) $< > $@
 
