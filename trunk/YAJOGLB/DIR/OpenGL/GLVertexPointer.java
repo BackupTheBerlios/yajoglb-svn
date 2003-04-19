39811d7c0b211e3bcd15acde75c95c1d8e147f3b
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
2003-04-19 13:40:08 +0000

    Added LGPL header.

--- trunk/YAJOGLB/DIR/OpenGL/GLVertexPointer.java
+++ trunk/YAJOGLB/DIR/OpenGL/GLVertexPointer.java
@@ -1,7 +1,30 @@
+/*
+  Support for vertex pointers
+
+  Copyright 2001, Robert Allan Zeh (razeh@yahoo.com)
+  7346 Lake Street #3W
+  River Forest, IL 60305
+ 
+  This library is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 2 of the
+  License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+  USA
+*/
+
 /* 
  * GLVertexPointer class
  *
- * $Id: GLVertexPointer.java,v 1.5 2002/07/21 16:55:47 razeh Exp $
+ * $Id: GLVertexPointer.java,v 1.6 2003/04/19 13:40:08 razeh Exp $
  *
  * Copyright 1997
  * Robert Allan Zeh (razeh@yahoo.com)
