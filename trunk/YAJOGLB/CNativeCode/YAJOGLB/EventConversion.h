1ba12b20f5722cca5c97416e721241cd8d66d4fa
razeh <razeh@6ff02c2e-7ee1-0310-a225-9b7d0aba9fe2>
2004-08-24 13:46:10 +0000

    Removed more pre-JAWT code

--- trunk/YAJOGLB/CNativeCode/YAJOGLB/EventConversion.h
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-
-/* This converts the windows idea of a key code into Java's idea of a
-   key code. */
-int win32CharacterToJava(JNIEnv *env, jclass class,
-			 int character);
-long GetModifiers(JNIEnv *env, jclass class,
-		  unsigned int nFlags);
-
